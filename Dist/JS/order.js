d058e4fee5eac0393d870c872b071d4214ed0e63
Umesh Pradhan <umesh.pradhan@rstartec.com>
2022-05-01 13:13:55 +0530

    add and save form data in table when we click on save changes button

---
+++ Dist/JS/order.js
@@ -0,0 +1,19 @@
+function saveData(){
+    var orderNumber = document.getElementById("txtOrderNumber").value;
+    var expiryDate = document.getElementById("txtDate").value;
+    var AccountName = document.getElementById("txtAccountName").value;
+    var phone = document.getElementById("txtPhone").value;
+    var shipAdd = document.getElementById("txtShipAddress").value;
+    console.log("saveData")
+
+    var orderObj = {
+        orderNumber,
+        expiryDate,
+        AccountName,
+        phone,
+        shipAdd
+
+    }
+    localStorage.setItem("orderRecord",JSON.stringify(orderObj));
+    document.getElementById("orderForm").reset();
+}
